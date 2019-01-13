54b6668154a3b6bc73a0d6b91e978fafa897fde9
Parker J. Rule <pjrule@me.com>
2019-01-13 02:43:37 -0500

    Added island and enclosure fusions (with unit tests)
    Former-commit-id: 9a7e8a0c53cfe8f67fff0add7b4801cc9765d290
    Former-commit-id: fb8ac99cab0c05d941ccf72ad5346d011fa2460a

---
+++ elbridge/fusions.py
@@ -0,0 +1,85 @@
+"""
+Methods for identifying VTDs that should always be allocated together.
+"""
+import numpy as np
+from typing    import Dict, List
+from geopandas import GeoDataFrame
+from libpysal.weights import W
+
+def fuse_islands(gdf: GeoDataFrame, adj: W) -> Dict[int, List[int]]:
+    """
+    Generates a fusion mapping for islands.
+    Islands are fused with the nearest inland VTD by centroid distance.
+
+    :param gdf: the GeoDataFrame with the VTDs.
+    :param adj: the VTDs' adjacency matrix.
+    """
+    closest = {}
+    if len(adj.islands) > 0:
+        # Find distances between island centroids and all VTD centroids. 
+        distances = np.zeros((len(adj.islands), len(gdf)))
+        centroids = [gdf.iloc[idx].geometry.centroid.coords[0] 
+                     for idx in adj.islands]
+        for idx, row in enumerate(gdf.geometry):
+            r_x, r_y = row.centroid.coords[0]
+            dist = [np.sqrt((x - r_x)**2 + (y - r_y)**2) for x, y in centroids]
+            distances[:,idx] = dist
+
+        # For each island, find the inland VTD with the closest nonzero 
+        # distance to the island.
+        for island_idx, island in enumerate(adj.islands):
+            closest_dist = np.max(distances[island_idx])
+            closest_idx = 0
+            for vtd_idx, dist in enumerate(list(distances[island_idx])):
+                if (dist > 0 and dist < closest_dist and 
+                    vtd_idx not in adj.islands):
+                    closest_dist = dist
+                    closest_idx = vtd_idx
+            closest[island] = closest_idx
+    return closest
+
+
+def fuse_enclosed(gdf: GeoDataFrame, adj: W) \
+     -> Dict[int, List[int]]:
+    """
+    Generates a fusion mapping for VTDs that are completely enclosed within
+    other VTDs.
+
+    :param gdf: the GeoDataFrame with the VTDs.
+    :param adj: the VTDs' adjacency matrix.
+    """
+    enclosed = {}
+    for idx, neighbors in enumerate(adj):
+        if len(neighbors[1]) == 1:
+            first_neighbor = list(neighbors[1].keys())[0]
+            inner = gdf.iloc[idx].geometry
+            outer = gdf.iloc[first_neighbor].geometry
+            if _is_enclosed(inner, outer):
+                enclosed[idx] = first_neighbor
+    return enclosed
+
+
+def _is_enclosed(inner, outer, tol: float = 0.001) -> bool:
+    """
+    Determines whether one VTD is completely enclosed by another VTD given
+    two VTDs' Shapely objects (Polygon, MultiPolygon, etc.) by comparing
+    their perimeters with the perimeters of their union.
+    
+    If one VTD is completely enclosed, we expect the perimeter of the union to
+    be _smaller_ than the perimeter of the outer VTD. This is because Shapely's
+    notion of perimeter (technically, length) takes the hole within the outer
+    VTD left by the inner VTD into account! This hole disappears in the union,
+    leaving only the outer perimeter. In the case of enclosure, we expect the
+    union's perimeter to decrease by exactly as much as the perimeter of the 
+    inner VTD due to this phenomenon. However, if the VTDs are overlapping or
+    adjacent, this conservation does not apply.
+
+    :param inner: a VTD with one neighbor (suspected to be enclosed).
+    :param outer: the VTD that neighbors ``inner``.
+    :param tol: the relative tolerance of the perimeter comparison
+        with respect to the perimeter of the union. The default of 0.1%
+        is based on an experiment with MGGG's Wisconsin shapefile.
+    """
+    union = inner.union(outer)
+    length_diff = abs(union.length - (outer.length - inner.length))
+    return length_diff < (tol * union.length)
---
+++ tests/test_fusions.py
@@ -0,0 +1,41 @@
+import pytest
+from geopandas import GeoDataFrame
+from shapely.geometry import box
+from libpysal.weights import Rook
+from elbridge.fusions import fuse_islands, fuse_enclosed
+
+# filter PySAL's disconnected observations warning (expected)
+@pytest.mark.filterwarnings("ignore::UserWarning") 
+def test_fuse_islands():
+    # Create a GeoDataFrame and an adjacency matrix for a map with
+    # a mainland (two adjacent VTDs) and an island (one free-standing VTD).
+    polys = [box(0, 0, 1, 1), box(1, 0, 2, 1), box(3, 3, 4, 4)]
+    gdf = GeoDataFrame({'geometry': polys})
+    adj = Rook.from_dataframe(gdf)
+
+    # The island is closest to the rightmost mainland VTD, so we expect
+    # a mapping from the island to that VTD.
+    assert fuse_islands(gdf, adj) == {2: 1}
+
+
+def test_fuse_enclosed_with_enclosed_vtds():
+    # The inner VTD is completely enclosed within the outer VTD.
+    # In this case, both VTDs are (loosely) squares, but the outer VTD
+    # has an inner VTD-sized hole in the middle.
+    inner = box(1, 1, 3, 3)
+    outer = box(0, 0, 4, 4).difference(inner)
+    gdf = GeoDataFrame({'geometry': [inner, outer]})
+    adj = Rook.from_dataframe(gdf)
+
+    # We expect a mapping from the inner VTD to the outer VTD.
+    assert fuse_enclosed(gdf, adj) == {0: 1}
+
+
+def test_fuse_enclosed_with_adjacent_vtds():
+    # Cnstruct two adjacent VTDs
+    polys = [box(0, 0, 1, 1), box(1, 0, 2, 1)]
+    gdf = GeoDataFrame({'geometry': polys})
+    adj = Rook.from_dataframe(gdf)
+
+    # We do not expect to find any enclosed VTDs.
+    assert fuse_enclosed(gdf, adj) == {}
