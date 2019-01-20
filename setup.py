742fc90e6e8f881e0b6bbdd2dc57144acc8eab5d
Parker J. Rule <pjrule@me.com>
2019-01-20 14:20:44 -0500

    Updated setup.py to support Linux builds

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import platform
 import os
 
-# https://stackoverflow.com/a/40031250
-os.environ["CFLAGS"] = "-O3 -std=c++11 -stdlib=libc++"
+os.environ['CFLAGS'] = '-O3 -std=c++11'
+if platform.system() == 'Darwin':
+    # It may be necessry to specify the use of libc++
+    # when building with clang on macOS.
+    # see https://stackoverflow.com/a/40031250
+    #     https://stackoverflow.com/a/50407611
+    os.environ['CFLAGS'] += '-stdlib=libc++'
 setup(
     name='elbridge',
-    ext_modules=cythonize(os.path.join("elbridge", "cgraph", "cgraph.pyx")),
+    ext_modules=cythonize(os.path.join('elbridge', 'cgraph', 'cgraph.pyx')),
     packages=['elbridge']
 )
